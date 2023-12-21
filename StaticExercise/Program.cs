79c1278c4043c464e761ed4a992f38c40f1c6edf
Victor Salinas <victorsalinas6@gmail.com>
2023-12-21 13:20:41 -0600

    Finished the exercise

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Math.Round(TempConverter.FahrenheitToCelsius(72)));
+
+            Console.WriteLine(Math.Round(TempConverter.CelsiusToFahrenheit(34)));
         }
     }
 }
